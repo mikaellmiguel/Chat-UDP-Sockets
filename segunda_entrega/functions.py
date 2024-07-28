498d2e9aa32651d504830795c89875ba3aad6000
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2024-07-28 15:49:27 -0300

    feat: criação de pacotes (header + payload)

--- segunda_entrega/functions.py
+++ segunda_entrega/functions.py
@@ -1,4 +1,5 @@
 import crcmod
+import struct
 
 def converterToTxt(username, texto, isServer = False):
 
@@ -23,3 +24,22 @@ def calcularChecksum(data:bytes):
 
     return checksum
 
+
+def criarPacote(msg:bytes, seq:int, ack:int):
+
+    # Payload da mensagem a ser enviada
+    payload = msg
+
+    # Header sem o Checksum 
+    pseudoHeader = struct.pack('!BB', seq, ack)
+
+    # Calculando o checksum com as informações do cabeçalho e o payload
+    checksum = calcularChecksum(pseudoHeader+payload)
+
+    # Header de 4 Bytes (Seq - 1 Byte, Ack - 1 Byte, Checksum - 2 Bytes)
+    header = struct.pack('!BBH', seq, ack, checksum)
+    
+    # Criando o pacote (Cabeçalho + Dados)
+    pacote = header+payload
+
+    return pacote  # Retornando o pacote já pronto para envio
